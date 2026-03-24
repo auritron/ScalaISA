7f70cdfabb5bdbadcb88666ae28448da25e3cadb
auritron <myself.anath@gmail.com>
2026-03-24 23:52:48 +0530

    instructions added

---
+++ ScaleISA_ins.md
@@ -0,0 +1,48 @@
+# Memory and Registers -
+LOAD  
+SEND  
+COPY  
+SET  
+SWAP  
+
+# Bitwise logic -
+NOT  
+AND  
+OR  
+XOR  
+
+# Shift and Rotate -
+STL  
+STR  
+RTL  
+RTR  
+
+# Arithmetic -
+ADD  
+SUB  
+MUL  
+DIV  
+MOD  
+NEG  
+
+# Comparison and Branching -
+CMP (flags - Z, S, C, O)  
+GOTO  
+WEQ  
+WGT  
+WLT  
+
+# Stacks and Sub-routines -
+CAL  
+RET  
+PUSH  
+POP  
+
+# I/O -
+PRINT  
+PRINTC  
+
+# Misc. and Debugging -
+END  
+CLRC  
+CLRR  
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+enum Instructions {
+
+    //memory and registers
+    LOAD,
+    SEND,
+    COPY,
+    SET,
+    SWAP,
+
+    //bitwise
+    NOT,
+    AND,
+    OR,
+    XOR,
+
+    //shift and rotation
+    STL,
+    STR,
+    RTL,
+    RTR,
+
+    //arithmetic
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    NEG,
+
+    //comparison and branching
+    CMP,
+    GOTO,
+    WEQ,
+    WGT,
+    WLT,
+
+    //stacks and sub-routines
+    CAL,
+    RET,
+    PUSH,
+    POP,
+
+    //i/o
+    PRINT,
+    PRINTC,
+
+    //misc. and debugging
+    END,
+    CLRC,
+    CLRR,
+
+};
